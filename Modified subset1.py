c6a364bd5426a1d63a152f0a16538a4a559d1005
rolrol-cx <116599823+rolrol-cx@users.noreply.github.com>
2022-12-02 20:43:09 -0500

    Update Modified subset1.py

--- Modified subset1.py
+++ Modified subset1.py
@@ -247,7 +247,12 @@ def main():
                                                 "(1 - " + str(len(bottles)) + "): ")
                 bottle_from_index = int(bottle_from_index)
                 bottle_to_index = int(bottle_to_index)
-
+            
+            if all_bottles_sorted(bottles):
+                for bottle in bottles: # for every item (bottle, which contain colors) in list of bottles created above
+                    print(str(bottle) + "\n") #actually displays bottles built in Bottle
+                print ("Congratulations! Ace that chem exam ;)")
+                level += 1
                 
             bottle_from: Bottle = bottles[bottle_from_index - 1] #A&V: new variable refering to class Bottle --> picks bottle (from bottles) chosen by user with corresponding index 
             bottle_to: Bottle = bottles[bottle_to_index - 1] #A&V: picks bottle (from bottles) to receive color --> from index chosen by user

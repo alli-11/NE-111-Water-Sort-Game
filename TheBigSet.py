d492efab1565e4d5c462edd2d4acf0a8176ee5f3
FatimaV2 <117775096+FatimaV2@users.noreply.github.com>
2022-12-06 19:49:44 -0500

    Update TheBigSet.py

--- TheBigSet.py
+++ TheBigSet.py
@@ -688,7 +688,7 @@ while True:
             """
             print()
             print("Welcome to 'Farmer's Hustle'.")
-            print("You are farmer. You have a pen of chickens and a cornfield. Recently you discovered that the jealous Farmer Dave, your nemesis, has released your chickens into your cornfield. \nWhen you went to catch them, \
+            print("You are farmer. You have a pen of chickens and a corn field. Recently you discovered that the jealous Farmer Dave, your nemesis, has released your chickens into your cornfield. \nWhen you went to catch them, \
         you spotted a family of foxes attempting to have chicken for lunch. Your job is to separate them all before \
         it's too late.")
             print ("---------")
